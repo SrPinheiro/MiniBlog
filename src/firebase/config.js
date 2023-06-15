b7e884b716e8e9f0331f8746500b2db79d18f5e6
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2023-06-14 23:41:11 -0300

    Security Update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,1 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA_EXw_zIFGatNfHJlS8nytMs4K7MXdK1A",
-  authDomain: "miniblog-78a26.firebaseapp.com",
-  projectId: "miniblog-78a26",
-  storageBucket: "miniblog-78a26.appspot.com",
-  messagingSenderId: "566753725639",
-  appId: "1:566753725639:web:66e5d788cb338bbe2902e2"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app)
-
-export {db};
+a
